d9c2296d82e1c8eb1f513d31e80fa826625c7ef7
Marcus Jakobsson <markusjacobsson93@gmail.com>
2025-11-05 08:46:51 +0000

    social_marketing: try to make it install

--- social_marketing/models/__init__.py
+++ social_marketing/models/__init__.py
@@ -2,9 +2,10 @@
 # Vertel AB AGPL-3
 
 # ~ from . import res_config_settings
+from . import social_marketing_post_template
 from . import social_marketing_account
 # ~ from . import social_marketing_post_template
-from . import social_marketing_post
+#from . import social_marketing_post
 # ~ from . import utm_campaign
 # ~ from . import utm_medium
 # ~ from . import utm_source
