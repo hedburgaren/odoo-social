a5adeeaab6efc996033d5c444f7d99ab0fd05cea
vertelbot <vertelbot@vertel.se>
2025-11-13 13:35:51 +0000

    odoobranchpfile odoo-social from 18.0. Do not p-file sync

--- social_marketing/models/social_marketing_account.py
+++ social_marketing/models/social_marketing_account.py
@@ -42,8 +42,6 @@ class SocialAccount(models.Model):
         return self.env.company
 
     name = fields.Char('Name', required=True)
-    # social_marketing_account_handle = fields.Char("Handle / Short Name",
-    #     help="Contains the social media handle of the person that created this account. E.g: '@odoo.official' for the 'Odoo' Twitter account")
     active = fields.Boolean("Active", default=True)
     media_id = fields.Many2one('social_marketing.media', string="Social Media", required=True, readonly=True,
        help="Related Social Media (Facebook, Twitter, ...).", ondelete='cascade')
